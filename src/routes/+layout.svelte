657f060c712db5f8b84585ddbd6af8b7e8eaa00a
Axel <axel.montlahuc@stjoavignon.com>
2023-09-04 11:48:36 +0200

    Added the layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,60 @@
+<script>
+    import main_logo from "$lib/img/MainLogo.webp"
+</script>
+
+<style>
+    :root {
+    --main-color: #f5f5f5;
+    --contrast-color: #0e0e0e;
+    --main-accentuation-color: #60e7b9;
+    --main-bg-color: #101928;
+    --dark-transparency: rgba(0, 0, 0, 0.2);
+    --accentuation-transparency: rgba(96, 231, 185, 0.6);
+}
+
+:global(body) {
+    background-color: var(--main-bg-color);
+    color: var(--main-color);
+    font-family: 'Roboto', sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+    margin: 15px;
+    height: 60px;
+}
+
+.main-logo {
+    height: 60px;
+}
+
+nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.nav-link {
+    color: var(--main-color);
+    text-decoration: none;
+    font-weight: 600;
+    padding: 0 15px;
+}
+</style>
+
+<header>
+    <img src={main_logo} class="main-logo" alt="Main Logo" />
+    <nav>
+        <a href="./index.html" class="nav-link">Home</a>
+        <a href="./install.html" class="nav-link">Install</a>
+        <a href="./simulator.html" class="nav-link">Simulator</a>
+        <a href="./aboutus.html" class="nav-link">About Us</a>
+    </nav>
+</header>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,158 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import banner from "$lib/img/HeroBanner.webp"
+    import logo from "$lib/img/MainLogo.webp"
+</script>
+
+<style>
+.hero-section {
+    background-image: url({banner});
+    background-size: cover;
+    height: calc(100vh - 90px);
+}
+
+.hero-logo {
+    position: absolute;
+    right: 20px;
+    bottom: calc(45px + 20px);
+    height: 100px;
+    margin: 0;
+    padding: 0;   
+}
+
+.hero-description {
+    position: absolute;
+    right: 20px;
+    bottom: 20px;
+    font-size: 2.4rem;
+    font-weight: 600;
+    color: var(--main-bg-color);
+    margin: 0;
+    padding: 0;
+}
+
+/*--------------------------------------------------------*/
+
+.features-section {
+    /*display: flex;
+    flex-direction: column;
+    justify-content: space-between;*/
+    margin: 0vw 3vw;
+    height: 100%;
+}
+
+.features-title {
+    font-size: 3rem;
+    font-weight: 600;
+    color: var(--main-color);
+    margin: 0;
+    padding: 0;
+}
+
+.features-title-bg {
+    position: relative;
+    width: fit-content;
+    height: fit-content;
+    left: 50%;
+    top: 100px;
+    transform: translateX(-50%);
+    padding: 25px;
+    border-radius: 8px;
+    background-color: var(--dark-transparency);
+}
+
+.features-list {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%);
+    padding-inline-start: 0px;
+    top: calc(100px + 30px + 50px);
+}
+
+.features-list-items {
+    text-decoration: none;
+    list-style: none;
+    border-radius: 8px;
+    background-color: var(--dark-transparency);
+    width: 18vw;
+    height: 18vw;
+    margin-bottom: 6vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+}
+
+.features-list-items:hover {
+    background-color: var(--accentuation-transparency);
+    transition: 0.3s;
+}
+
+.features-list-items-title {
+    font-size: 150%;
+    font-weight: 600;
+    color: var(--main-color);
+    margin: 0px 40px;
+    margin-top: 60px;
+    padding: 0;
+    text-align: center;
+}
+
+.features-list-items-description {
+    font-size: 100%;
+    font-weight: 400;
+    color: var(--main-color);
+    margin: 0px 40px;
+    margin-bottom: 60px;
+    padding: 0;
+    text-align: justify;
+}
+</style>
+
+<section class="hero-section">
+    <img src={logo} class="hero-logo" alt="Logo" />
+    <h3 class="hero-description">The future of calculation</h3>
+</section>
+<section class="features-section">
+    <div class="features-title-bg">
+        <h2 class="features-title">Sigma Features</h2>
+    </div>
+    <ul class="features-list">
+        <li class="features-list-items">
+            <h4 class="features-list-items-title">Enhanced Python</h4>
+            <p class="features-list-items-description">Sigma adds to Python new modules like os or ulab</p>
+        </li>
+        <li class="features-list-items">
+            <h4 class="features-list-items-title">Themes</h4>
+            <p class="features-list-items-description">Sigma has a variety of themes to allow you to have the best experience with your numworks</p>
+        </li>
+        <li class="features-list-items">
+            <h4 class="features-list-items-title">Gameboy</h4>
+            <p class="features-list-items-description">Sigma integrates natively a GameBoy emulator to allow you to run any .gb file</p>
+        </li>
+        <li class="features-list-items">
+            <h4 class="features-list-items-title">Customizable</h4>
+            <p class="features-list-items-description">Sigma also integrates a ton of new settings to improve th user experience</p>
+        </li>
+    </ul>
+    <ul class="features-list">
+        <li class="features-list-items">
+            <h4 class="features-list-items-title">Symbolic Calculation</h4>
+            <p class="features-list-items-description">Symbolic computation was removed from Epsilon. Sigma reintroduces that feature.</p>
+        </li>
+        <li class="features-list-items">
+            <h4 class="features-list-items-title">Dual Boot</h4>
+            <p class="features-list-items-description">Sigma uses Upsilon's bootloader, which allows you to have up to 3 partitions to boot on. </p>
+        </li>
+        <li class="features-list-items">
+            <h4 class="features-list-items-title">Protection against lock</h4>
+            <p class="features-list-items-description">The custom bootloader prevents you to update your calculator towards an OS that lock your calculator</p>
+        </li>
+        <li class="features-list-items">
+            <h4 class="features-list-items-title">Plenty of other features</h4>
+            <p class="features-list-items-description">Check the releases to see all the features</p>
+        </li>
+    </ul>
+</section>
